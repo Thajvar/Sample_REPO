c9f6ef000913e7fac06cee41c43a260dfac7c91b
Thajvar <tajofficial2803@gmail.com>
2023-08-28 20:56:55 +0530

    NewFile

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+const person = {
+fname: 'Thajvar', lname: 'usman',
+address: 'old washermenpet', phone: 8608816180
+}
+console.log(person );
+console.log(person.fname.toLowerCase());
+
+console.log(person.phone);
+
+
+let exm = 10 + 20+ 'thaj' + 20 + 20;
+console.log(exm);
+function fun(p1,p2) {
+return p1 + p2;
+
+}
+let x = fun(10,90);
+console.log(x);
+
+    function  fcal(x) {
+     return (5/9) * (x-32);
+    }
+
+let fval = 'The Temp is ' +fcal(77)+ ' degree';
+console.log(fval);
+
+let date = display.Date();
+console.log(date);
+
